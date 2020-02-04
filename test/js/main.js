d332a65f86891850940e0074009624ecc1668db1
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-02-04 11:00:03 +0300

    Add files via upload

---
+++ test/js/main.js
@@ -0,0 +1,366 @@
+$(document).ready(function(){
+    /* Service 1 */	
+    $('.service__btn_first').click(function() {
+    $(".service__bg_first").addClass("service__bg-click");
+    $(".service__hidden_first").addClass("service__show");
+    $('.service__btn_close_first').delay(190).queue(function(){
+           $(this).addClass('service__show-btn');
+           $(this).dequeue()
+       });
+    $(".service__text_first").addClass("service__text-click");
+    $(".service__text-btn_first").addClass("service__hidden_first");
+    $(".service__span_first").addClass("service__hidden_first");
+    $(".service__btn_first").addClass("service__hidden_first");
+    $(".service__title_first").addClass("service__title-click");
+   
+   
+         });
+    $('.service__btn_close_first').click(function () {
+    $(".service__btn_close_first").removeClass("service__show-btn");
+    $(".service__bg_first").removeClass("service__bg-click");
+    $(".service__hidden_first").removeClass("service__show");
+    $(".service__text_first").removeClass("service__text-click");
+    $(".service__btn_first").removeClass("service__hidden_first");
+    $(".service__title_first").removeClass("service__title-click");
+    $(".service__span_first").removeClass("service__hidden_first");
+         });
+    /* Service 2 */
+    $('.service__btn_second').click(function() {
+    $(".service__bg_second").addClass("service__bg-click");
+    $(".service__hidden_second").addClass("service__show");
+    $('.service__btn_close_second').delay(190).queue(function(){
+           $(this).addClass('service__show-btn');
+           $(this).dequeue()
+       });
+    $(".service__text_second").addClass("service__text-click_second");
+    $(".service__text-btn_second").addClass("service__hidden_second");
+    $(".service__span_second").addClass("service__hidden_second");
+    $(".service__btn_second").addClass("service__hidden_second");
+    $(".service__title_second").addClass("service__title-click");
+   
+   
+         });
+    $('.service__btn_close_second').click(function () {
+    $(".service__btn_close_second").removeClass("service__show-btn");
+    $(".service__bg_second").removeClass("service__bg-click");
+    $(".service__hidden_second").removeClass("service__show");
+    $(".service__text_second").removeClass("service__text-click_second");
+    $(".service__btn_second").removeClass("service__hidden_second");
+    $(".service__title_second").removeClass("service__title-click");
+    $(".service__span_second").removeClass("service__hidden_second");
+         });
+    /* Service 3 */
+    $('.service__btn_third').click(function() {
+    $(".service__bg_third").addClass("service__bg-click_third");
+    $(".service__hidden_third").addClass("service__show");
+    $('.service__btn_close_third').delay(190).queue(function(){
+           $(this).addClass('service__show-btn');
+           $(this).dequeue()
+       });
+    $(".service__text_third").addClass("service__text-click_third");
+    $(".service__text-btn_third").addClass("service__hidden_third");
+    $(".service__span_third").addClass("service__hidden_third");
+    $(".service__btn_third").addClass("service__hidden_third");
+    $(".service__title_third").addClass("service__title-click");
+   
+   
+         });
+    $('.service__btn_close_third').click(function () {
+    $(".service__btn_close_third").removeClass("service__show-btn");
+    $(".service__bg_third").removeClass("service__bg-click_third");
+    $(".service__hidden_third").removeClass("service__show");
+    $(".service__text_third").removeClass("service__text-click_third");
+    $(".service__btn_third").removeClass("service__hidden_third");
+    $(".service__title_third").removeClass("service__title-click");
+    $(".service__span_third").removeClass("service__hidden_third");
+         });
+    /* Service 4 */
+    $('.service__btn_fourth').click(function() {
+    $(".service__bg_fourth").addClass("service__bg-click_fourth");
+    $(".service__hidden_fourth").addClass("service__show");
+    $('.service__btn_close_fourth').delay(190).queue(function(){
+           $(this).addClass('service__show-btn');
+           $(this).dequeue()
+       });
+    $(".service__text_fourth").addClass("service__text-click_fourth");
+    $(".service__text-btn_fourth").addClass("service__hidden_fourth");
+    $(".service__span_fourth").addClass("service__hidden_fourth");
+    $(".service__btn_fourth").addClass("service__hidden_fourth");
+    $(".service__title_fourth").addClass("service__title-click");
+   
+   
+         });
+    $('.service__btn_close_fourth').click(function () {
+    $(".service__btn_close_fourth").removeClass("service__show-btn");
+    $(".service__bg_fourth").removeClass("service__bg-click_fourth");
+    $(".service__hidden_fourth").removeClass("service__show");
+    $(".service__text_fourth").removeClass("service__text-click_fourth");
+    $(".service__btn_fourth").removeClass("service__hidden_fourth");
+    $(".service__title_fourth").removeClass("service__title-click");
+    $(".service__span_fourth").removeClass("service__hidden_fourth");
+         });
+    /* Service 5 */
+    $('.service__btn_fifth').click(function() {
+    $(".service__bg_fifth").addClass("service__bg-click_fifth");
+    $(".service__hidden_fifth").addClass("service__show");
+    $('.service__btn_close_fifth').delay(190).queue(function(){
+           $(this).addClass('service__show-btn');
+           $(this).dequeue()
+       });
+    $(".service__text_fifth").addClass("service__text-click_fifth");
+    $(".service__text-btn_fifth").addClass("service__hidden_fifth");
+    $(".service__span_fifth").addClass("service__hidden_fifth");
+    $(".service__btn_fifth").addClass("service__hidden_fifth");
+    $(".service__title_fifth").addClass("service__title-click");
+   
+   
+         });
+    $('.service__btn_close_fifth').click(function () {
+    $(".service__btn_close_fifth").removeClass("service__show-btn");
+    $(".service__bg_fifth").removeClass("service__bg-click_fifth");
+    $(".service__hidden_fifth").removeClass("service__show");
+    $(".service__text_fifth").removeClass("service__text-click_fifth");
+    $(".service__btn_fifth").removeClass("service__hidden_fifth");
+    $(".service__title_fifth").removeClass("service__title-click");
+    $(".service__span_fifth").removeClass("service__hidden_fifth");
+         });
+    /* Service 6 */
+    $('.service__btn_sixth').click(function() {
+    $(".service__bg_sixth").addClass("service__bg-click_sixth");
+    $(".service__hidden_sixth").addClass("service__show");
+    $('.service__btn_close_sixth').delay(190).queue(function(){
+           $(this).addClass('service__show-btn');
+           $(this).dequeue()
+       });
+    $(".service__text_sixth").addClass("service__text-click_sixth");
+    $(".service__text-btn_sixth").addClass("service__hidden_sixth");
+    $(".service__span_sixth").addClass("service__hidden_sixth");
+    $(".service__btn_sixth").addClass("service__hidden_sixth");
+    $(".service__title_sixth").addClass("service__title-click");
+   
+   
+         });
+    $('.service__btn_close_sixth').click(function () {
+    $(".service__btn_close_sixth").removeClass("service__show-btn");
+    $(".service__bg_sixth").removeClass("service__bg-click_sixth");
+    $(".service__hidden_sixth").removeClass("service__show");
+    $(".service__text_sixth").removeClass("service__text-click_sixth");
+    $(".service__btn_sixth").removeClass("service__hidden_sixth");
+    $(".service__title_sixth").removeClass("service__title-click");
+    $(".service__span_sixth").removeClass("service__hidden_sixth");
+         });
+   /* Input(remove "*"") */
+    $('.contacts__input_first').keypress(function () {
+    $(".contacts__required_first").addClass("none");
+   });
+   
+    $('.contacts__input_second').keypress(function () {
+    $(".contacts__required_second").addClass("none");
+   });
+   
+    $('.contacts__input_third').keypress(function () {
+    $(".contacts__required_third").addClass("none");
+   });
+   
+    $('.contacts__text').keypress(function () {
+    $(".contacts__required_fourth").addClass("none");
+   });
+   
+   /* Input(add "*"") */
+   
+   $('.contacts__input_first').blur(function()         
+   {                   
+       if( !$(this).val() ) {                     
+            $(".contacts__required_first").removeClass("none");   
+       }
+   });
+   
+   $('.contacts__input_second').blur(function()         
+   {                   
+       if( !$(this).val() ) {                     
+            $(".contacts__required_second").removeClass("none");   
+       }
+   });
+   
+   $('.contacts__input_third').blur(function()         
+   {                   
+       if( !$(this).val() ) {                     
+            $(".contacts__required_third").removeClass("none");   
+       }
+   });
+   
+   $('.contacts__text').blur(function()         
+   {                   
+       if( !$(this).val() ) {                     
+            $(".contacts__required_fourth").removeClass("none");   
+       }
+   });
+   
+   
+   /*Carousel Swipe */
+   
+   var hammer = new Hammer(document.querySelector('.carousel'));
+   var $carousel = $(".carousel").carousel({"interval":0});
+   hammer.get("swipe");
+   hammer.on("swipeleft", function(){
+       $carousel.carousel("next");
+   });
+   hammer.on("swiperight", function(){
+       $carousel.carousel("prev");
+   });
+   
+   }); 
+   
+    /* Team-info */
+   
+   
+   var target = $('.team__info_first');
+   var targetPos = target.offset().top;
+   var winHeight = $(window).height();
+   var scrollToElem = targetPos - winHeight;
+   $(window).scroll(function(){
+     var winScrollTop = $(this).scrollTop();
+     if(winScrollTop > scrollToElem){
+        $(".team__info_first").toggleClass("d-block-info");
+        $(".team__info_first").addClass("animated");
+        $(".team__info_first").addClass("slower");
+        $(".team__info_first").addClass("fadeIn");
+     }
+     });
+   
+   var target = $('.team__info_second');
+   var targetPos = target.offset().top;
+   var winHeight = $(window).height();
+   var scrollToElem = targetPos - winHeight;
+   $(window).scroll(function(){
+     var winScrollTop = $(this).scrollTop();
+     if(winScrollTop > scrollToElem){
+        $(".team__info_second").toggleClass("d-block-info");
+        $(".team__info_second").addClass("animated");
+        $(".team__info_second").addClass("slower");
+        $(".team__info_second").addClass("fadeIn");
+     }
+     });
+   
+     var target = $('.team__info_third');
+   var targetPos = target.offset().top;
+   var winHeight = $(window).height();
+   var scrollToElem = targetPos - winHeight;
+   $(window).scroll(function(){
+     var winScrollTop = $(this).scrollTop();
+     if(winScrollTop > scrollToElem){
+        $(".team__info_third").toggleClass("d-block-info");
+        $(".team__info_third").addClass("animated");
+        $(".team__info_third").addClass("slower");
+        $(".team__info_third").addClass("fadeIn");
+     }
+   });
+   
+     var target = $('.team__info_fourth');
+   var targetPos = target.offset().top;
+   var winHeight = $(window).height();
+   var scrollToElem = targetPos - winHeight;
+   $(window).scroll(function(){
+     var winScrollTop = $(this).scrollTop();
+     if(winScrollTop > scrollToElem){
+        $(".team__info_fourth").toggleClass("d-block-info");
+        $(".team__info_fourth").addClass("animated");
+        $(".team__info_fourth").addClass("slower");
+        $(".team__info_fourth").addClass("fadeIn");
+     }
+   });
+   
+     var target = $('.team__info_fifth');
+   var targetPos = target.offset().top;
+   var winHeight = $(window).height();
+   var scrollToElem = targetPos - winHeight;
+   $(window).scroll(function(){
+     var winScrollTop = $(this).scrollTop();
+     if(winScrollTop > scrollToElem){
+        $(".team__info_fifth").toggleClass("d-block-info");
+        $(".team__info_fifth").addClass("animated");
+        $(".team__info_fifth").addClass("slower");
+        $(".team__info_fifth").addClass("fadeIn");
+     }
+   });
+   
+     var target = $('.team__info_sixth');
+   var targetPos = target.offset().top;
+   var winHeight = $(window).height();
+   var scrollToElem = targetPos - winHeight;
+   $(window).scroll(function(){
+     var winScrollTop = $(this).scrollTop();
+     if(winScrollTop > scrollToElem){
+        $(".team__info_sixth").toggleClass("d-block-info");
+        $(".team__info_sixth").addClass("animated");
+        $(".team__info_sixth").addClass("slower");
+        $(".team__info_sixth").addClass("fadeIn");
+     }
+   });
+   
+    $('.team__info_first').click(function () {
+    $(".team__description_first").addClass("d-block-info");
+        });
+   
+    $('.team__info_second').click(function () {
+    $(".team__description_second").addClass("d-block-info");
+        });
+   
+     $('.team__info_third').click(function () {
+    $(".team__description_third").addClass("d-block-info");
+        });
+   
+      $('.team__info_fourth').click(function () {
+    $(".team__description_fourth").addClass("d-block-info");
+        });
+   
+       $('.team__info_fifth').click(function () {
+    $(".team__description_fifth").addClass("d-block-info");
+        });
+   
+        $('.team__info_sixth').click(function () {
+    $(".team__description_sixth").addClass("d-block-info");
+        });
+   
+   
+    $('.container').click(function () {
+    $(".team__description_first").removeClass("d-block-info");
+        });
+   
+    $('.team__info_second').click(function () {
+    $(".team__description_second").removeClass("d-block-info");
+        });
+   
+     $('.team__info_third').click(function () {
+    $(".team__description_third").removeClass("d-block-info");
+        });
+   
+      $('.team__info_fourth').click(function () {
+    $(".team__description_fourth").removeClass("d-block-info");
+        });
+   
+       $('.team__info_fifth').click(function () {
+    $(".team__description_fifth").removeClass("d-block-info");
+        });
+   
+        $('.team__info_sixth').click(function () {
+    $(".team__description_sixth").removeClass("d-block-info");
+        });
+   
+   /* Navigation */
+   
+   $('.menu-burger').click(function () {
+           $('.modal__overlay').addClass('show_overlay');
+           $('.modal__body').addClass('show_body');
+           $('.modal').addClass('show_body');
+           $('.modal').addClass('fadeIn');
+           $('.menu__burger').addClass('none');
+       });
+       $('.modal__overlay').click(function() {
+           $('.modal__overlay').removeClass('show_overlay');
+           $('.modal__body').removeClass('show_body');
+           $('.modal').removeClass('show_body');
+           $('.modal').removeClass('fadeIn');
+           $('.menu-burger').removeClass('none');
+       });
+   
